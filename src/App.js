725a460f0af7ecbaa0f7a6a86386dcc1821cbaff
berger <robertberger.dev@gmail.com>
2023-03-09 18:13:52 -0500

    placed Navigation component into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Navigation from './Navigation';
 
 function App() {
   return (
-<BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-  </Routes>
-</BrowserRouter>
+  <>
+    <Navigation />
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
