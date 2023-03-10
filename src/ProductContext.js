a46aa7ab1f6ea42babb5961c8c1739542d5e6983
berger <robertberger.dev@gmail.com>
2023-03-09 20:58:34 -0500

    setup ProductContex.js

--- src/ProductContext.js
+++ src/ProductContext.js
@@ -1,3 +1,52 @@
-import React from "react";
-const ProductContext = React.createContext();
-export default ProductContext;
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const ProductContext = createContext()
+
+export const ProductProvider = (props) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function getProducts() {
+            await refreshProducts()
+        }
+        getProducts()
+    }, []);
+
+    function refreshProducts() {
+        return axios.get("http://localhost/products")
+        .then(response => {
+            setProduct(response.data)
+        })
+    }
+
+    function getProduct(id) {
+        return products.find(product => product.id === parseInt(id))
+    }
+
+    function deleteProduct(id) {
+
+    }
+
+    function addProduct(id) {
+
+    }
+
+    function updateProduct(id) {
+
+    }
+
+    return (
+        <ProductContext.Provider
+            value={{
+                products,
+                getProduct,
+                deleteProduct,
+                addProduct,
+                updateProduct
+            }}
+        >
+            {props.children}
+        </ProductContext.Provider>
+    )
+}
