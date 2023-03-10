0ff8055f32aa67adece960e8b83da884eaf49874
berger <robertberger.dev@gmail.com>
2023-03-10 10:43:29 -0500

    removed an incorrect import

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,6 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import { NavbarBrand } from "react-bootstrap";
 
 function Navigation(props) {
     return (
